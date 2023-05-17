ce131abd14677fe62534772c8e23574dafb38ee0
MilanTuryna <miloslavturyna@seznam.cz>
2023-05-17 17:44:10 +0200

    added column

--- app/Model/Database/Repository/Dynamic/Entity/DynamicAttribute.php
+++ app/Model/Database/Repository/Dynamic/Entity/DynamicAttribute.php
@@ -13,11 +13,12 @@ use App\Model\Database\Entity;
 class DynamicAttribute extends Entity
 {
     const name = "name";
-    const data_type = "data_type", placeholder_expression_id = "placeholder_expression_id", preset_val = "preset_val";
+    const data_type = "data_type", placeholder_expression_id = "placeholder_expression_id", preset_val = "preset_val", entity_id = "entity_id";
 
     public string $name;
     public string $data_type;
     public int $placeholder_expression_id;
+    public int $entity_id;
     public string $preset_val;
     public int $id;
 }
