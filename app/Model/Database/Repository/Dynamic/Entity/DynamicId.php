bd989e8dadc9ee50409b157f2bee64ad3f4db913
MilanTuryna <miloslavturyna@seznam.cz>
2023-05-19 12:44:43 +0200

    created createRowUnique() method, changed columns (my typo)

--- app/Model/Database/Repository/Dynamic/Entity/DynamicId.php
+++ app/Model/Database/Repository/Dynamic/Entity/DynamicId.php
@@ -5,6 +5,7 @@ namespace App\Model\Database\Repository\Dynamic\Entity;
 
 
 use App\Model\Database\Entity;
+use Exception;
 
 /**
  * Class DynamicId
@@ -12,15 +13,28 @@ use App\Model\Database\Entity;
  */
 class DynamicId extends Entity
 {
-    const name = "name";
     const created = "created";
-    const description = "description";
     const row_unique = "row_unique";
+    const entity_id = "entity_id";
     const id = "id";
 
-    public string $name;
     public string $created;
-    public string $description;
     public int $id;
+    public int $entity_id;
     public string $row_unique;
+
+    /**
+     * @throws Exception
+     */
+    public static function createRowUnique($len = 13): string {
+        // used solution on: https://www.php.net/manual/en/function.uniqid.php#120123
+        if (function_exists("random_bytes")) {
+            $bytes = random_bytes(ceil($len / 2));
+        } elseif (function_exists("openssl_random_pseudo_bytes")) {
+            $bytes = openssl_random_pseudo_bytes(ceil($len / 2));
+        } else {
+            throw new Exception("no cryptographically secure random function available");
+        }
+        return substr(bin2hex($bytes), 0, $len);
+    }
 }
