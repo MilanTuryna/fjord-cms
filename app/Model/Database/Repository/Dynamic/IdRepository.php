6b722e46bad9c316c1fa70e7505b4619ea95205b
MilanTuryna <miloslavturyna@seznam.cz>
2023-05-19 12:40:38 +0200

    configured IdRepository and ValueRepository

--- app/Model/Database/Repository/Dynamic/IdRepository.php
+++ app/Model/Database/Repository/Dynamic/IdRepository.php
@@ -4,7 +4,21 @@
 namespace App\Model\Database\Repository\Dynamic;
 
 
-class IdRepository
-{
+use App\Model\Database\AbstractRepository;
+use Nette\Database\Explorer;
 
+/**
+ * Class IdRepository
+ * @package App\Model\Database\Repository\Dynamic
+ */
+class IdRepository extends AbstractRepository
+{
+    /**
+     * IdRepository constructor.
+     * @param Explorer $explorer
+     */
+    public function __construct(Explorer $explorer)
+    {
+        parent::__construct("fjord_dynamic_ids", $explorer);
+    }
 }
--- app/Model/Database/Repository/Dynamic/ValueRepository.php
+++ app/Model/Database/Repository/Dynamic/ValueRepository.php
@@ -4,7 +4,17 @@
 namespace App\Model\Database\Repository\Dynamic;
 
 
-class ValueRepository
-{
+use App\Model\Database\AbstractRepository;
+use Nette\Database\Explorer;
 
+class ValueRepository extends AbstractRepository
+{
+    /**
+     * ValueRepository constructor.
+     * @param Explorer $explorer
+     */
+    public function __construct(Explorer $explorer)
+    {
+        parent::__construct("fjord_dynamic_values", $explorer);
+    }
 }
