8f1c9f6dcf87a1998dff52900b80ad90e85716e1
MilanTuryna <miloslavturyna@seznam.cz>
2023-05-19 14:24:23 +0200

    set Form as abstract class + added methods + created RepositoryForm

--- app/Forms/Form.php
+++ app/Forms/Form.php
@@ -4,7 +4,38 @@
 namespace App\Forms;
 
 
-class Form
-{
+use Nette\Application\UI\Presenter;
 
+abstract class Form
+{
+    public function __construct(protected Presenter $presenter) {
+    }
+
+    /**
+     * @param array $values ([input_name => value])
+     */
+    public function setDefaultValues(\Nette\Application\UI\Form &$form, array $values)
+    {
+        foreach ($values as $k => $v) $form[$k]->setDefaultValue($v);
+    }
+
+    /**
+     * @param $class
+     * @return \Nette\Application\UI\Form
+     */
+    public static function staticCreate($class): \Nette\Application\UI\Form {
+        $form = new \Nette\Application\UI\Form();
+        $form->onSuccess[] = [$class, 'success'];
+        $form->onError[] = function() use ($form, $class) {
+            foreach($form->getErrors() as $error) $class->presenter->flashMessage($error, FlashMessages::ERROR);
+        };
+        return $form;
+    }
+
+    /**
+     * @return \Nette\Application\UI\Form
+     */
+    public function create(): \Nette\Application\UI\Form {
+        return self::staticCreate($this);
+    }
 }
