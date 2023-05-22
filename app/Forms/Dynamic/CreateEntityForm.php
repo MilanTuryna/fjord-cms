829df3426b2a6d7ff5988de2cbe430d16759b522
MilanTuryna <miloslavturyna@seznam.cz>
2023-05-22 15:50:17 +0200

    created entityform + createentityform

--- app/Forms/Dynamic/CreateEntityForm.php
+++ app/Forms/Dynamic/CreateEntityForm.php
@@ -4,11 +4,44 @@
 namespace App\Forms\Dynamic;
 
 
+use App\Forms\Dynamic\Data\EntityFormData;
+use App\Forms\FlashMessages;
+use App\Forms\FormRedirect;
+use App\Model\Database\EAV\DynamicEntityFactory;
+use App\Model\Database\Repository\Dynamic\Entity\DynamicEntity;
+use JetBrains\PhpStorm\NoReturn;
+use JetBrains\PhpStorm\Pure;
+use Nette\Application\AbortException;
 use Nette\Application\UI\Form;
+use Nette\Application\UI\Presenter;
 
-class CreateEntityForm
+/**
+ * Class CreateEntityForm
+ * @package App\Forms\Dynamic
+ */
+class CreateEntityForm extends EntityForm
 {
-    public function __construct() {
+    /**
+     * CreateEntityForm constructor.
+     * @param Presenter $presenter
+     * @param DynamicEntityFactory $dynamicEntityFactory
+     * @param FormRedirect $redirect
+     */
+    #[Pure] public function __construct(Presenter $presenter, private DynamicEntityFactory $dynamicEntityFactory, private FormRedirect $redirect)
+    {
+        parent::__construct($presenter);
+    }
 
+    /**
+     * @throws AbortException
+     */
+    #[NoReturn] public function success(Form $form, EntityFormData $entityFormData): void {
+        $entity = new DynamicEntity();
+        $entity->name = $entityFormData->entity_name;
+        $entity->description = $entityFormData->entity_description;
+        $entity->created = new \DateTime();
+        $entityId = $this->dynamicEntityFactory->createEntity($entity, $entityFormData->attributes);
+        $this->presenter->flashMessage("Entita byla úpěšně vytvořena", FlashMessages::SUCCESS);
+        $this->redirect->presenter($this->presenter, $entityId);
     }
 }
---
+++ app/Forms/Dynamic/EntityForm.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Forms\Dynamic;
+
+
+use App\Forms\Dynamic\Data\AttributeData;
+use App\Forms\Form;;
+use Nette\Forms\Container;
+
+/**
+ * Class EntityForm
+ * @package App\Forms\Dynamic
+ */
+class EntityForm extends Form
+{
+    /**
+     * @return \Nette\Application\UI\Form
+     */
+    public function create(): \Nette\Application\UI\Form
+    {
+        $form = parent::create();
+        $form->addText("entity_name", "Název entity/položky")
+            ->setRequired(true);
+        $form->addText("entity_description", "Popis položky")->setRequired(true);
+        $maxAttributes = 50;
+        $attrMultiplier = $form->addMultiplier("attributes", function (Container $container, \Nette\Forms\Form $form) use(&$attrMultiplier) {
+            $container->addText(AttributeData::name, "Název atributu")->setRequired(true);
+            $container->addSelect(AttributeData::data_type, "Datový typ", AttributeData::DATA_TYPES)->setRequired(true);
+            $container->addText(AttributeData::description, "Popis atributu");
+            $container->addText(AttributeData::placeholder, "HTML Placeholder");
+            // If AttributeData::generate_value and ::preset_val then use prese_val;
+            $container->addSelect(AttributeData::generate_value, "Vygenerované hodnoty",
+                AttributeData::GENERATED_VALUES)->setRequired(false)->setPrompt("Vyber generovanou hodnotu");
+            $container->addSelect(AttributeData::preset_val, "Přednastavení hodnoty")->setRequired(false);
+            $container->addCheckbox(AttributeData::required, "Je atribut povinný?")->setRequired(false);
+            $attrMultiplier->addRemoveButton("Odebrat atribut")->addClass('btn btn-danger');
+        }, 1, $maxAttributes);
+        $attrMultiplier->createButton("Přidat krok")->addClass('btn btn-dark w-100');
+        $form->addSubmit("submit", "Vytvořit novou entitu");
+        return $form;
+    }
+}
