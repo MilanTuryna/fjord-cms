0d8c7b17085d733ae5cedb2351c09b1d16285cce
MilanTuryna <miloslavturyna@seznam.cz>
2023-05-22 16:08:08 +0200

    created method getActualSettinsg

--- app/Model/Database/Repository/Settings/GlobalSettingsRepository.php
+++ app/Model/Database/Repository/Settings/GlobalSettingsRepository.php
@@ -4,14 +4,15 @@
 namespace App\Model\Database\Repository\Settings;
 
 
-use App\Model\Database\AbstractRepository;
+use App\Model\Database\Repository;
 use Nette\Database\Explorer;
+use Nette\Database\Table\ActiveRow;
 
 /**
  * Class GlobalSettingsRepository
  * @package App\Model\Database\Repository\Settings
  */
-class GlobalSettingsRepository extends AbstractRepository
+class GlobalSettingsRepository extends Repository
 {
     /**
      * GlobalSettingsRepository constructor.
@@ -20,5 +21,13 @@ class GlobalSettingsRepository extends AbstractRepository
     public function __construct(Explorer $explorer)
     {
         parent::__construct("fjord_global_settings", $explorer);
+    }
+
+    /**
+     * @return ActiveRow|Repository\Settings\Entity\GlobalSettings
+     */
+    public function getActualSettings(): ?ActiveRow
+    {
+        return $this->explorer->table($this->table)->order("id DESC")->limit(1)->fetch();
     }
 }
