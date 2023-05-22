e7ea722bdf42d7d9f2527c064680bf18954aee4f
MilanTuryna <miloslavturyna@seznam.cz>
2023-05-22 15:41:30 +0200

    created presenter method in FormRedirect

--- app/Forms/FormRedirect.php
+++ app/Forms/FormRedirect.php
@@ -3,6 +3,10 @@
 
 namespace App\Forms;
 
+use JetBrains\PhpStorm\NoReturn;
+use Nette\Application\AbortException;
+use Nette\Application\UI\Presenter;
+
 /**
  * Class FormRedirect
  * @package App\Forms
@@ -25,5 +29,16 @@ class FormRedirect
     public function __construct(string $route, array $args = []) {
         $this->route = $route;
         $this->args = $args;
+    }
+
+    /**
+     * @param Presenter $presenter
+     * @param int|null $lastInsertedId
+     * @throws AbortException
+     */
+    #[NoReturn] public function presenter(Presenter $presenter, ?int $lastInsertedId = null): void {
+        $presenter->redirect($this->route, array_map(function ($arg) use($lastInsertedId) {
+            return $arg === FormRedirect::LAST_INSERT_ID ? $lastInsertedId : $arg;
+        }, $this->args));
     }
 }
