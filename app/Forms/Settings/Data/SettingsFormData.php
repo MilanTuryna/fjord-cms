139d8ad260e30c47020ee2522e0b90d5732a0399
MilanTuryna <miloslavturyna@seznam.cz>
2023-05-24 20:04:14 +0200

    Created SettingsForm

---
+++ app/Forms/Settings/Data/SettingsFormData.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Forms\Settings\Data;
+
+
+use App\Model\Database\Repository\Settings\Entity\GlobalSettings;
+
+/**
+ * Class SettingsFormData
+ * @package App\Forms\Settings\Data
+ */
+class SettingsFormData extends GlobalSettings
+{
+
+}
---
+++ app/Forms/Settings/SettingsForm.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace App\Forms\Settings;
+
+
+use App\Forms\FormMessage;
+use App\Forms\FormOption;
+use App\Forms\FormRedirect;
+use App\Forms\RepositoryForm;
+use App\Forms\Settings\Data\SettingsFormData;
+use App\Model\Database\DataRegulation;
+use App\Model\Database\Repository;
+use JetBrains\PhpStorm\Pure;
+use Nette\Application\AbortException;
+use Nette\Application\UI\Form;
+use Nette\Application\UI\InvalidLinkException;
+use Nette\Application\UI\Presenter;
+
+/**
+ * Class SettingsForm
+ * @package App\Forms\Settings
+ */
+class SettingsForm extends RepositoryForm
+{
+    private Repository\Settings\GlobalSettingsRepository $settingsRepository;
+
+    /**
+     * SettingsForm constructor.
+     * @param Presenter $presenter
+     * @param Repository\Settings\GlobalSettingsRepository $repository
+     * @param FormRedirect $formRedirect
+     * @param int $admin_id
+     */
+    #[Pure] public function __construct(protected Presenter $presenter, protected Repository\Settings\GlobalSettingsRepository $repository, private FormRedirect $formRedirect, private int $admin_id)
+    {
+        parent::__construct($this->presenter, $this->repository);
+
+        $this->settingsRepository = $repository;
+    }
+
+    public function create(): \Nette\Application\UI\Form
+    {
+        $form = parent::create();
+        $actualSettings = $this->settingsRepository->getActualSettings();
+
+        $form->addText("app_name", "Název aplikace")
+            ->setMaxLength(DataRegulation::TITLE)
+            ->setDefaultValue($actualSettings->app_name ?? "")->setRequired(true);
+        $form->addText("app_author", "Autoři aplikace")
+            ->setMaxLength(DataRegulation::TITLE)
+            ->setDefaultValue($actualSettings->app_author ?? "")->setRequired(true);
+        $form->addText("app_keywords", "Klíčová slova")
+            ->setOption(FormOption::OPTION_NOTE, "Bez diakritiky, ve formátu: music, play, split, sound")
+            ->setMaxLength(DataRegulation::DESCRIPTION)
+            ->setDefaultValue($actualSettings->app_keywords ?? "")->setRequired(true);
+        return $form;
+    }
+
+    /**
+     * @param Form $form
+     * @param SettingsFormData $data
+     * @throws AbortException
+     * @throws InvalidLinkException
+     */
+    public function success(Form $form, SettingsFormData $data): void {
+        $data->admin_id = $this->admin_id;
+        $this->successTemplate($form, $data, new FormMessage(
+            "Nastavení bylo úspěšně aktualizováno a ve všech službách aplikováno.",
+            "Vyskytla se neznámá chyba v databázi a nebylo možné aktualizovat nastavení."));
+    }
+}
