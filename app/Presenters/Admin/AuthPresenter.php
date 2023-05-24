b6474fd960713a3ff37180889552578fd669d9f8
MilanTuryna <miloslavturyna@seznam.cz>
2023-05-24 19:48:48 +0200

    Created AuthPresenter

---
+++ app/Presenters/Admin/AuthPresenter.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Presenters\Admin;
+
+
+// BasePresenter because AdminBasePresenter is verifying session automatically
+use App\Forms\Auth\SignInForm;
+use App\Model\Security\Auth\AdminAuthenticator;
+use App\Presenters\BasePresenter;
+use Nette\Application\AbortException;
+use Nette\Application\UI\Form;
+
+/**
+ * Class AuthPresenter
+ * @package App\Presenters\Admin
+ */
+class AuthPresenter extends BasePresenter
+{
+    private AdminAuthenticator $adminAuthenticator;
+
+    /**
+     * AdminAuthPresenter constructor.
+     * @param AdminAuthenticator $adminAuthenticator
+     */
+    public function __construct(AdminAuthenticator $adminAuthenticator) {
+        parent::__construct();
+
+        $this->adminAuthenticator = $adminAuthenticator;
+    }
+
+    /**
+     * @throws AbortException
+     */
+    public function startup() {
+        parent::startup();
+        // admin is logged
+        if($this->adminAuthenticator->getId()) $this->redirect(":Admin:Overview:home");
+    }
+
+    /**
+     * @return Form
+     */
+    public function createComponentSignInForm(): Form {
+        return (new SignInForm($this, $this->adminAuthenticator, ":Admin:Overview:home"))->create();
+    }
+}
