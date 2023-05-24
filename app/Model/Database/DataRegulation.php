b8fcfa3cbb91fcdfc8b17c6b33e7da847377165b
MilanTuryna <miloslavturyna@seznam.cz>
2023-05-24 18:16:11 +0200

    created DataRegulation (for column lenghts)

---
+++ app/Model/Database/DataRegulation.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Model\Database;
+
+
+interface DataRegulation
+{
+    const URL = 200;
+    const EMAIl_LENGTH = 320;
+    const TITLE = 50;
+    const URI = 50;
+    const DESCRIPTION = 200;
+    const KEYWORDS = 500;
+    const VERSION = 20;
+    const TIMEZONE = 30;
+    const COUNTRY_CODE = 20;
+    const G_ANALYTICS = 8;
+    const ICO = 9;
+    const CSS_COLOR = 200;
+}
