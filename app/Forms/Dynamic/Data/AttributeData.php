82c7a3b9242ebf452985d95a8541bf967a8dcee7
NB_MilanT <milan@turyna.eu>
2023-05-25 12:27:24 +0200

    used DataType

--- app/Forms/Dynamic/Data/AttributeData.php
+++ app/Forms/Dynamic/Data/AttributeData.php
@@ -4,17 +4,18 @@
 namespace App\Forms\Dynamic\Data;
 
 
+use App\Model\Database\EAV\DataType;
 use App\Model\Database\EAV\Translations\TranslatedValue;
 use App\Model\Database\Repository\Dynamic\Entity\DynamicAttribute;
 
 class AttributeData extends DynamicAttribute
 {
     const DATA_TYPES = [
-        "integer" => "Celé číslo",
-        "float" => "Číslo s desetinným místem",
-        "string" => "Textový řetězec",
-        TranslatedValue::class => "Textový řetězec s překladem",
-        "bool" => "ANO/NE"
+        DataType::INTEGER  => "Celé číslo",
+        DataType::FLOAT  => "Číslo s desetinným místem",
+        DataType::STRING => "Textový řetězec",
+        DataType::TRANSLATED_VALUE => "Textový řetězec s překladem",
+        DataType::BOOL => "ANO/NE"
     ];
 
     const GENERATED_VALUES = [
