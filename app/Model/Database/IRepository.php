c0caac073d0ccf6b848df838952cc9fbe352bd2c
NB_MilanT <milan@turyna.eu>
2023-05-25 09:34:12 +0200

    added some methods to IRepository

--- app/Model/Database/IRepository.php
+++ app/Model/Database/IRepository.php
@@ -4,5 +4,7 @@ namespace App\Model\Database;
 
 interface IRepository
 {
+    public function insert(array $data): int;
+    public function updateById(int $id, iterable $data): int;
     public function deleteById(int $id): int;
 }
