eb094754ee97677ffe4e2db03ff6eace26026e46
NB_MilanT <milan@turyna.eu>
2023-05-25 13:03:17 +0200

    created EntityPresenter (need to create SpecificEntity forms)

--- app/Presenters/Admin/Dynamic/EntityPresenter.php
+++ app/Presenters/Admin/Dynamic/EntityPresenter.php
@@ -4,9 +4,52 @@
 namespace App\Presenters\Admin\Dynamic;
 
 
+use App\Forms\FormMessage;
+use App\Model\Admin\Permissions\Specific\AdminPermissions;
+use App\Model\Database\EAV\DynamicEntityFactory;
+use App\Model\Database\EAV\EAVRepository;
+use App\Model\Database\EAV\Exceptions\EntityNotFoundException;
+use App\Model\Security\Auth\AdminAuthenticator;
 use App\Presenters\AdminBasePresenter;
+use JetBrains\PhpStorm\NoReturn;
+use Nette\Application\AbortException;
 
 class EntityPresenter extends AdminBasePresenter
 {
+    /**
+     * @param AdminAuthenticator $adminAuthenticator
+     * @param string $permissionNode
+     * @param EAVRepository $EAVRepository
+     * @param DynamicEntityFactory $dynamicEntityFactory
+     */
+    public function __construct(AdminAuthenticator $adminAuthenticator, string $permissionNode = AdminPermissions::DYNAMIC_ENTITY_EDIT, protected EAVRepository $EAVRepository, protected DynamicEntityFactory $dynamicEntityFactory)
+    {
+        parent::__construct($adminAuthenticator, $permissionNode);
+    }
 
+    public function renderList(string $entityName) {
+        $this->template->rows = $this->EAVRepository->findAll();
+    }
+
+    /**
+     * @throws EntityNotFoundException
+     */
+    public function renderView(string $entityName, string $rowUnique) {
+        $EAVRepository = $this->dynamicEntityFactory->getEntityRepository($entityName);
+        $this->template->entityName = $entityName;
+        $this->template->rowUnique = $rowUnique;
+        $this->template->row = $EAVRepository->findByUnique($rowUnique);
+    }
+
+    /**
+     * @throws AbortException|EntityNotFoundException
+     */
+    #[NoReturn] public function renderRemove(string $entityName, string $rowUnique) {
+        $EAVRepository = $this->dynamicEntityFactory->getEntityRepository($entityName);
+        $this->prepareActionRemove($EAVRepository, $rowUnique, new FormMessage("$entityName (ID: $rowUnique) byl úspěšně odebrán.", "$entityName (ID: $rowUnique) nebyl z neznámého důvodu odebrán."), "list");
+    }
+
+    public function renderNew(string $entityName) {
+        $this->template->entityName = $entityName;
+    }
 }
