47a4d5e1516a718b702de26e9d276e7b84075df2
NB_MilanT <milan@turyna.eu>
2023-06-06 11:30:32 +0200

    OS compability (using directory separtor )

--- www/index.php
+++ www/index.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 
 $configurator = App\Bootstrap::boot();
 $container = $configurator->createContainer();
