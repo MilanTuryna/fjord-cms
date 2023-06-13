8fe3f837de784869e11555840355de0351dfa579
MilanTuryna <miloslavturyna@seznam.cz>
2023-06-13 19:48:23 +0200

    passing entity list into template

--- app/Presenters/Admin/Internal/MainPresenter.php
+++ app/Presenters/Admin/Internal/MainPresenter.php
@@ -4,9 +4,20 @@
 namespace App\Presenters\Admin\Internal;
 
 
+use App\Model\Admin\Permissions\Utils;
+use App\Model\Database\Repository\Dynamic\EntityRepository;
+use App\Model\Security\Auth\AdminAuthenticator;
 use App\Presenters\AdminBasePresenter;
 
 class MainPresenter extends AdminBasePresenter
 {
+    public function __construct(AdminAuthenticator $adminAuthenticator, string $permissionNode = Utils::SPECIAL_WITHOUT_PERMISSION)
+    {
+        parent::__construct($adminAuthenticator, $permissionNode);
+    }
 
+    public function renderHome() {
+        $entityList = $this->template->entityList = $this->entityRepository->findAll()->fetchAll();
+        $this->template->entityCount = count($entityList);
+    }
 }
