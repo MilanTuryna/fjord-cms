2207c4418520eb9490ce8037b483d1efbd49466e
MilanTuryna <miloslavturyna@seznam.cz>
2023-06-13 20:30:36 +0200

    inheritance

--- app/Model/Database/Repository/SMTP/MailRepository.php
+++ app/Model/Database/Repository/SMTP/MailRepository.php
@@ -4,7 +4,22 @@
 namespace App\Model\Database\Repository\SMTP;
 
 
-class MailRepository
-{
+use App\Model\Database\Repository;
+use JetBrains\PhpStorm\Pure;
+use Nette\Database\Explorer;
 
+/**
+ * Class MailRepository
+ * @package App\Model\Database\Repository\SMTP
+ */
+class MailRepository extends Repository
+{
+    /**
+     * MailRepository constructor.
+     * @param Explorer $explorer
+     */
+    #[Pure] public function __construct(Explorer $explorer)
+    {
+        parent::__construct("fjord_smtp_mails", $explorer);
+    }
 }
--- app/Model/Database/Repository/SMTP/ServerRepository.php
+++ app/Model/Database/Repository/SMTP/ServerRepository.php
@@ -4,7 +4,17 @@
 namespace App\Model\Database\Repository\SMTP;
 
 
-class ServerRepository
-{
+use JetBrains\PhpStorm\Pure;
+use Nette\Database\Explorer;
 
+/**
+ * Class ServerRepository
+ * @package App\Model\Database\Repository\SMTP
+ */
+class ServerRepository extends \App\Model\Database\Repository
+{
+    #[Pure] public function __construct(Explorer $explorer)
+    {
+        parent::__construct("fjord_smtp_servers", $explorer);
+    }
 }
