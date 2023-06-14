683e2d41c3b1e629f1ec5ba531708862181d9580
NB_MilanT <milan@turyna.eu>
2023-06-14 11:02:55 +0200

    created blank classes for template form

---
+++ app/Forms/Template/Data/TemplateFormData.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Forms\Template\Data;
+
+use App\Model\Database\Repository\Template\Entity\Template;
+
+class TemplateFormData extends Template
+{
+
+}
---
+++ app/Forms/Template/InstallTemplateForm.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Forms\Template;
+
+class InstallTemplateForm
+{
+
+}
