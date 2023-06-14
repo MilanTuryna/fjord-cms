924be43a71560b25c27a5231d6e50eccda6f0864
NB_MilanT <milan@turyna.eu>
2023-06-14 10:40:39 +0200

    passing some vars to template

--- app/Presenters/Admin/Internal/MainPresenter.php
+++ app/Presenters/Admin/Internal/MainPresenter.php
@@ -6,12 +6,15 @@ namespace App\Presenters\Admin\Internal;
 
 use App\Model\Admin\Permissions\Utils;
 use App\Model\Database\Repository\Dynamic\EntityRepository;
+use App\Model\Database\Repository\SMTP\ServerRepository;
+use App\Model\Database\Repository\Template\Entity\Template;
+use App\Model\Database\Repository\Template\TemplateRepository;
 use App\Model\Security\Auth\AdminAuthenticator;
 use App\Presenters\AdminBasePresenter;
 
 class MainPresenter extends AdminBasePresenter
 {
-    public function __construct(AdminAuthenticator $adminAuthenticator, string $permissionNode = Utils::SPECIAL_WITHOUT_PERMISSION)
+    public function __construct(AdminAuthenticator $adminAuthenticator, private ServerRepository $serverRepository, private TemplateRepository $templateRepository, string $permissionNode = Utils::SPECIAL_WITHOUT_PERMISSION)
     {
         parent::__construct($adminAuthenticator, $permissionNode);
     }
@@ -19,5 +22,9 @@ class MainPresenter extends AdminBasePresenter
     public function renderHome() {
         $entityList = $this->template->entityList = $this->entityRepository->findAll()->fetchAll();
         $this->template->entityCount = count($entityList);
+        $templates = $this->template->templates = $this->templateRepository->findAll()->fetchAll();
+        $this->template->templateCount = count($templates);
+        $this->template->usedTemplate = $this->templateRepository->findByColumn(Template::used, true)->fetch();
+        $this->template->emailServers = $this->serverRepository->findAll()->fetchAll();
     }
 }
