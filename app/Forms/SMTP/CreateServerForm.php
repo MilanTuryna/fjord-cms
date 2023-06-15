047c6a653c1c6899a5c2f81b6431d50c5f8a6421
MilanTuryna <miloslavturyna@seznam.cz>
2023-06-15 17:33:38 +0200

    Create/Edit serverform

--- app/Forms/SMTP/CreateServerForm.php
+++ app/Forms/SMTP/CreateServerForm.php
@@ -4,7 +4,36 @@
 namespace App\Forms\SMTP;
 
 
-class CreateServerForm
-{
+use App\Forms\FormMessage;
+use App\Forms\FormRedirect;
+use App\Forms\SMTP\Data\ServerFormData;
+use App\Model\Database\Repository;
+use JetBrains\PhpStorm\Pure;
+use Nette\Application\AbortException;
+use Nette\Application\UI\Form;
+use Nette\Application\UI\InvalidLinkException;
+use Nette\Application\UI\Presenter;
 
+/**
+ * Class CreateServerForm
+ * @package App\Forms\SMTP
+ */
+class CreateServerForm extends ServerForm
+{
+    #[Pure] public function __construct(Presenter $presenter, Repository\SMTP\ServerRepository $serverRepository, private FormRedirect $formRedirect)
+    {
+        parent::__construct($presenter, $serverRepository);
+    }
+
+    /**
+     * @param Form $form
+     * @param ServerFormData $serverFormData
+     * @throws AbortException
+     * @throws InvalidLinkException
+     */
+    public function success(Form $form, ServerFormData $serverFormData): void
+    {
+        parent::success($form, $serverFormData);
+        $this->successTemplate($form, $serverFormData->iterable(), new FormMessage("Daný SMTP byl úspěšně aktualizován.", "Daný SMTP nemohl být z neznámého důovdu aktualizován"), $this->formRedirect);
+    }
 }
--- app/Forms/SMTP/EditServerForm.php
+++ app/Forms/SMTP/EditServerForm.php
@@ -4,7 +4,43 @@
 namespace App\Forms\SMTP;
 
 
-class EditServerForm
-{
+use App\Forms\FormMessage;
+use App\Forms\FormRedirect;
+use App\Forms\SMTP\Data\ServerFormData;
+use App\Model\Database\Repository;
+use JetBrains\PhpStorm\Pure;
+use Nette\Application\AbortException;
+use Nette\Application\UI\Form;
+use Nette\Application\UI\InvalidLinkException;
+use Nette\Application\UI\Presenter;
 
+/**
+ * Class EditServerForm
+ * @package App\Forms\SMTP
+ */
+class EditServerForm extends ServerForm
+{
+    #[Pure] public function __construct(Presenter $presenter, private Repository\SMTP\ServerRepository $serverRepository, private int $server_id)
+    {
+        parent::__construct($presenter, $serverRepository);
+    }
+
+    /**
+     * @return Form
+     */
+    public function create(): Form {
+        $form = parent::create();
+        $activeRow = $this->serverRepository->findById($this->server_id);
+        return $this::createEditForm($form, $activeRow);
+    }
+
+    /**
+     * @throws InvalidLinkException
+     * @throws AbortException
+     */
+    public function success(Form $form, ServerFormData $serverFormData): void
+    {
+        parent::success($form, $serverFormData);
+        $this->successTemplate($form, $serverFormData->iterable(), new FormMessage("SMTP server byl úspěšně aktualizován.", "SMTP server byl úspěšně aktualizován."), new FormRedirect("this"), $this->server_id);
+    }
 }
