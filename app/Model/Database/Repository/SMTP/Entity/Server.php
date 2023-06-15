59c5c2d2ebd20e8116fc31502b7fbfd6e7964af8
MilanTuryna <miloslavturyna@seznam.cz>
2023-06-15 18:41:25 +0200

    column names

--- app/Model/Database/Repository/SMTP/Entity/Server.php
+++ app/Model/Database/Repository/SMTP/Entity/Server.php
@@ -13,7 +13,7 @@ use App\Model\Database\Entity;
 class Server extends Entity
 {
     const name = "name";
-    const server_email = "server_email", server_password = "server_password", server_host = "server_host", receiver_server_email = "receiver_server_email";
+    const server_email = "server_email", server_password = "server_password", server_host = "server_host", receiver_email = "receiver_server_email";
 
     public string $name;
 
@@ -21,7 +21,7 @@ class Server extends Entity
     public string $server_password;
     public string $server_host;
 
-    public string $receiver_server_email;
+    public string $receiver_email;
 
     public int $id;
 }
