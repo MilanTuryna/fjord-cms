7dd2a6ec6ea4c2f83c9fb889ce5991e1827a8c4c
MilanTuryna <miloslavturyna@seznam.cz>
2023-06-15 18:23:35 +0200

    created component create/editServer

--- app/Presenters/Admin/Internal/SMTPPresenter.php
+++ app/Presenters/Admin/Internal/SMTPPresenter.php
@@ -6,6 +6,8 @@ namespace App\Presenters\Admin\Internal;
 
 use App\Forms\FormMessage;
 use App\Forms\FormRedirect;
+use App\Forms\SMTP\CreateServerForm;
+use App\Forms\SMTP\EditServerForm;
 use App\Model\Admin\Permissions\Specific\AdminPermissions;
 use App\Model\Database\Repository\SMTP\Entity\Mail;
 use App\Model\Database\Repository\SMTP\MailRepository;
@@ -14,6 +16,8 @@ use App\Model\Security\Auth\AdminAuthenticator;
 use App\Presenters\AdminBasePresenter;
 use JetBrains\PhpStorm\NoReturn;
 use Nette\Application\AbortException;
+use Nette\Application\UI\Form;
+use Nette\Application\UI\Multiplier;
 
 /**
  * Class SMTPPresenter
@@ -60,5 +64,21 @@ class SMTPPresenter extends AdminBasePresenter
      */
     #[NoReturn] public function actionRemove(int $id): void {
         $this->prepareActionRemove($this->serverRepository, $id, new FormMessage("Daný SMTP server byl úspěšně odstraněn. Zkontrolujte si prosím, zda máte všechny zveřejněné formuláře na webu funkční.", "Daný SMTP server nemohl být z neznámého důvodu odstraněn."), new FormRedirect("list"));
+    }
+
+    /**
+     * @return Form
+     */
+    public function createComponentCreateServerForm(): Form {
+        return (new CreateServerForm($this, $this->serverRepository, new FormRedirect("view",  [FormRedirect::LAST_INSERT_ID])))->create();
+    }
+
+    /**
+     * @return Multiplier
+     */
+    public function createComponentEditServerForm(): Multiplier {
+        return new Multiplier(function ($server_id) {
+            return (new EditServerForm($this, $this->serverRepository, (int)$server_id));
+        });
     }
 }
