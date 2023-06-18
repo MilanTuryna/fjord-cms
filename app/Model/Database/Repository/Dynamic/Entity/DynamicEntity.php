6505bdcbcd412048386ccf4d385549c5c2838ce8
MilanTuryna <miloslavturyna@seznam.cz>
2023-06-18 17:20:41 +0200

    typo in column names

--- app/Model/Database/Repository/Dynamic/Entity/DynamicEntity.php
+++ app/Model/Database/Repository/Dynamic/Entity/DynamicEntity.php
@@ -12,12 +12,14 @@ use App\Model\Database\Entity;
  */
 class DynamicEntity extends Entity
 {
-    const name = "name", declension = "declension", created = "created", description = "description", last_edit = "last_edit", id = "id";
+    const name = "name", declension = "declension", created = "created", description = "description", generated_by = "generated_by", edited = "edited", id = "id";
 
     public string $name;
     public string $declension; // parse to three declension strings ["článek", "články", "článků"]
     public string $created;
+    public string $edited;
     public string $description;
     public string $last_edit;
+    public string $generated_by;
     public int $id;
 }
