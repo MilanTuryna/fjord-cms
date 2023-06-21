37b49cf4eb8e624447de81b76ae1bbfe69bd19b0
MilanTuryna <miloslavturyna@seznam.cz>
2023-06-21 17:11:12 +0200

    created FormatUtils

---
+++ app/Utils/FormatUtils.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Utils;
+
+
+class FormatUtils
+{
+    /**
+     * @param $bytes
+     * @param int $precision
+     * @return int
+     */
+    public static function formatBytes($bytes, int $precision = 0): int {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        // Uncomment one of the following alternatives
+        // $bytes /= pow(1024, $pow);
+        // $bytes /= (1 << (10 * $pow));
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
+}
