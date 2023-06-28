63d1a9b4d5dae4c8794e31d66124d6c8d903524e
MilanTuryna <miloslavturyna@seznam.cz>
2023-06-28 22:36:00 +0200

    constructor

--- app/Presenters/Admin/Internal/TemplatePresenter.php
+++ app/Presenters/Admin/Internal/TemplatePresenter.php
@@ -15,6 +15,7 @@ use App\Model\Database\Repository\Admin\Entity\AccessLog;
 use App\Model\Database\Repository\Dynamic\EntityRepository;
 use App\Model\Database\Repository\Template\AuthorRepository;
 use App\Model\Database\Repository\Template\PageRepository;
+use App\Model\Database\Repository\Template\PageVariableRepository;
 use App\Model\Database\Repository\Template\TemplateRepository;
 use App\Model\FileSystem\Templating\TemplateUploadDataProvider;
 use App\Model\Security\Auth\AdminAuthenticator;
@@ -36,7 +37,10 @@ class TemplatePresenter extends AdminBasePresenter
      * @param PageRepository $pageRepository
      * @param string $permissionNode
      */
-    public function __construct(AdminAuthenticator $adminAuthenticator, private TemplateRepository $templateRepository, private TemplateUploadDataProvider $templateUploadDataProvider, private DynamicEntityFactory $dynamicEntityFactory, private AuthorRepository $authorRepository, private PageRepository $pageRepository, string $permissionNode = AdminPermissions::DEVELOPER_SETTINGS)
+    public function __construct(AdminAuthenticator $adminAuthenticator, private TemplateRepository $templateRepository,
+                                private TemplateUploadDataProvider $templateUploadDataProvider, private DynamicEntityFactory $dynamicEntityFactory,
+                                private AuthorRepository $authorRepository, private PageRepository $pageRepository, private PageVariableRepository $pageVariableRepository,
+                                string $permissionNode = AdminPermissions::DEVELOPER_SETTINGS)
     {
         parent::__construct($adminAuthenticator, $permissionNode);
     }
@@ -52,7 +56,7 @@ class TemplatePresenter extends AdminBasePresenter
         $templates = $this->template->templates = $this->templateRepository->findAll()->fetchAll();
         $authors = [];
         foreach ($templates as $template) {
-            $authors[$template->author_id] = $template->related("author_id")->fetch();
+            $authors[$template->author_id] = $template->related("fjord_template_author.id")->fetch();
         }
         $this->template->authors = $authors;
     }
@@ -63,7 +67,7 @@ class TemplatePresenter extends AdminBasePresenter
      */
     public function renderView(int $id): void {
         $templateRow = $this->template->templateRow = $this->templateRepository->findById($id);
-        $this->template->templateAuthor = $templateRow->related("author_id")->fetch();
+        $this->template->templateAuthor = $templateRow->related("fjord_template_author.id")->fetch();
     }
 
     /**
@@ -71,7 +75,7 @@ class TemplatePresenter extends AdminBasePresenter
      */
     public function createComponentInstallTemplateForm(): Form {
         return (new InstallTemplateForm($this, $this->templateRepository, $this->templateUploadDataProvider,
-            $this->dynamicEntityFactory, $this->authorRepository, $this->pageRepository, new FormRedirect("view", [FormRedirect::LAST_INSERT_ID])))->create();
+            $this->dynamicEntityFactory, $this->authorRepository, $this->pageRepository, $this->pageVariableRepository, new FormRedirect("view", [FormRedirect::LAST_INSERT_ID])))->create();
     }
 
     /**
