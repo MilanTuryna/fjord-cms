c2f4f56e0f5d4a7ac15b6b5e16e17b82646508d3
MilanTuryna <miloslavturyna@seznam.cz>
2023-06-29 16:56:42 +0200

    column

--- app/Model/Database/Repository/Template/Entity/Template.php
+++ app/Model/Database/Repository/Template/Entity/Template.php
@@ -12,14 +12,14 @@ use App\Model\Database\Entity;
  */
 class Template extends Entity
 {
-    const title = "title", author_id = "author_id", used = "used", help_link = "help_link", description = "description", version = "version", created = "created", edited = "edited"
+    const title = "title", author_id = "author_id", used = "used", website = "website", description = "description", version = "version", created = "created", edited = "edited"
 , id = "id", dirname = "dirname", error404 = "error404";
 
     public string $title;
     public string $dirname;
     public string $error404; // representing relative path from index.json to page 404 (Not Found)
     public int $author_id;
-    public string $help_link;
+    public string $website;
     public string $description;
     public string $version;
     public string $created;
