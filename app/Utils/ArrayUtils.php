7702ee2cc747870e464ee2d889d4545142be9d5a
MilanTuryna <miloslavturyna@seznam.cz>
2023-07-07 19:40:55 +0200

    desc asc orders

--- app/Utils/ArrayUtils.php
+++ app/Utils/ArrayUtils.php
@@ -18,4 +18,29 @@ class ArrayUtils
         }
         return $result;
     }
+
+    /**
+     * @param array $array
+     * @param mixed $key
+     * @return array
+     */
+    public static function descOrder(array $array, mixed $key): array {
+        usort($array, function ($f, $s) use ($key) {
+            return $s[$key] <=> $f[$key];
+        });
+        return $array;
+    }
+
+    /**
+     * @param array $array
+     * @param mixed $key
+     * @return array
+     */
+    public static function ascOrder(array $array, mixed $key): array {
+        usort($array, function ($f, $s) use ($key) {
+            return $f[$key] <=> $s[$key];
+        });
+        bdump($array);
+        return $array;
+    }
 }
