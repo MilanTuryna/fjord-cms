ba9c4867c5c32cb4135c39ef6cf0c917990e899e
MilanTuryna <miloslavturyna@seznam.cz>
2023-07-15 10:58:34 +0200

    passed FFMpeg provider to component forms

--- app/Presenters/Admin/Gallery/MainPresenter.php
+++ app/Presenters/Admin/Gallery/MainPresenter.php
@@ -15,6 +15,7 @@ use App\Model\Database\Repository\Gallery\Entity\Gallery;
 use App\Model\Database\Repository\Gallery\Entity\GalleryItem;
 use App\Model\Database\Repository\Gallery\GalleryRepository;
 use App\Model\Database\Repository\Gallery\ItemsRepository;
+use App\Model\DI\FFMpegProvider;
 use App\Model\FileSystem\Gallery\Exceptions\ImageNotExistException;
 use App\Model\FileSystem\Gallery\GalleryDataProvider;
 use App\Model\FileSystem\Gallery\GalleryFacadeFactory;
@@ -41,9 +42,11 @@ class MainPresenter extends AdminBasePresenter
      * @param GalleryRepository $galleryRepository
      * @param GalleryDataProvider $galleryDataProvider
      * @param GalleryFacadeFactory $galleryFacadeFactory
+     * @param FFMpegProvider $FFMpegProvider
      * @param string $permissionNode
      */
-    public function __construct(AdminAuthenticator $adminAuthenticator, protected ItemsRepository $itemsRepository, protected GalleryRepository $galleryRepository, protected GalleryDataProvider $galleryDataProvider, protected GalleryFacadeFactory $galleryFacadeFactory, string $permissionNode = AdminPermissions::GALLERY_EDIT)
+    public function __construct(AdminAuthenticator $adminAuthenticator, protected ItemsRepository $itemsRepository, protected GalleryRepository $galleryRepository, protected GalleryDataProvider $galleryDataProvider, protected GalleryFacadeFactory $galleryFacadeFactory, protected FFMpegProvider $FFMpegProvider,
+                                string $permissionNode = AdminPermissions::GALLERY_EDIT)
     {
         parent::__construct($adminAuthenticator, $permissionNode);
     }
@@ -121,7 +124,9 @@ class MainPresenter extends AdminBasePresenter
         $image = $this->itemsRepository->findById($imageId);
         if($image) {
             $this->itemsRepository->deleteById($imageId);
-            $facade->getGalleryUploadManager()->deleteUpload($image->compressed_file);
+            $galleryUploadManager = $facade->getGalleryUploadManager();
+            $galleryUploadManager->deleteUpload($image->compressed_file);
+            if($galleryUploadManager::isVideo($image->compressed_file)) $galleryUploadManager->removeVideoFrame($image->compressed_file);
             $this->flashMessage("Daný obrázek byl odstraněn úspěšně.", FlashMessages::SUCCESS);
         } else {
             $this->flashMessage("Daný obrázek nebyl odstraněn, protože neexistuje. Někde možná nastala chyba.", FlashMessages::ERROR);
@@ -144,12 +149,12 @@ class MainPresenter extends AdminBasePresenter
      * @return Form
      */
     public function createComponentCreateGalleryForm(): Form {
-        return (new CreateGalleryForm($this, $this->galleryRepository, $this->itemsRepository, $this->galleryDataProvider, $this->admin->id, new FormRedirect("view", [FormRedirect::LAST_INSERT_ID])))->create();
+        return (new CreateGalleryForm($this, $this->galleryRepository, $this->itemsRepository, $this->galleryDataProvider, $this->admin->id, new FormRedirect("view", [FormRedirect::LAST_INSERT_ID]), $this->FFMpegProvider))->create();
     }
 
     public function createComponentEditGalleryForm(): Multiplier {
         return new Multiplier(function ($id) {
-            return (new EditGalleryForm($this, $this->galleryRepository, $this->itemsRepository, $this->galleryDataProvider, $this->admin->id, (int)$id, new FormRedirect("remove", [$id])))->create();
+            return (new EditGalleryForm($this, $this->galleryRepository, $this->itemsRepository, $this->galleryDataProvider, $this->admin->id, (int)$id, new FormRedirect("remove", [$id]), $this->FFMpegProvider))->create();
         });
     }
 }
